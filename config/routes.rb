b36d01ce775879ba5fa71471dc07df174b88a694
brandenmittra <branden@univrs.co.uk>
2015-11-20 16:28:01 +0000

    hello world

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 OneMonthStripePayments::Application.routes.draw do
+
+  root 'pages#home'
+
+  get 'home' => 'pages#home'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
